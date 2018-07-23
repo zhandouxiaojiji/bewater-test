9540534d5766046b88923fa1011a5b56af6e7a07
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2018-07-23 17:49:34 +0800

    test robot

--- pressure/script/service/robot.lua
+++ pressure/script/service/robot.lua
@@ -1,10 +1,12 @@
 local skynet = require "skynet"
 local socket = require "websocketclient"
+local ws_proto = require "ws.proto"
 
 local function send_text(fd, data)
     local fin = true
     local opcode = 0x1
     local finbit, mask_bit
+    local mask_outgoing = true
     if fin then
         finbit = 0x80
     else
@@ -13,6 +15,7 @@ local function send_text(fd, data)
 
     local frame = string.pack("B", finbit | opcode)
     local l = #data
+    print("payload data:", l)
 
     if mask_outgoing then
         mask_bit = 0x80
@@ -33,9 +36,20 @@ local function send_text(fd, data)
     socket.send(fd, frame)
 end
 
+local function send_text2(fd, data)
+    local frame = ws_proto.build_frame(true, 0x1, #data, data, true)
+    socket.send(fd, frame)
+end
+
 skynet.start(function()		
     --local fd = assert(socket.connect("127.0.0.1", 8653))
     local fd = assert(socket.connect("127.0.0.1", 8002))
-    send_text(fd, "hello") 
+    --send_text(fd, '{"grant_type":"client_credential","appid":"wx6c83f709500b39a8","secret":"95f6bbe6a6a03226c01fa8a5ae276280"}') 
+    local str = ""
+    for i = 1, 97 do
+        str = str .. "a"
+    end
+    str = str .. "bbb"
+    send_text2(fd, str)
 end)
 
