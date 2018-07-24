c19b450403975443ec4cac8bcf8cf7c8afce7694
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2018-07-24 21:19:01 +0800

    test

--- pressure/script/service/master.lua
+++ pressure/script/service/master.lua
@@ -2,11 +2,11 @@ local skynet    = require "skynet"
 local conf      = require "conf"
 
 skynet.start(function()
-    for i = 1, 1 do
+    for i = 1, 16 do
         local robot = skynet.newservice("ws/robot", "player_t")
         --skynet.call(robot, "lua", "start", "ws://127.0.0.1:8002")
         skynet.call(robot, "lua", "start", "ws://127.0.0.1:8655")
-        skynet.sleep(10)
+        skynet.sleep(100)
     end
 
     --[[local player_t = require "player_t"
