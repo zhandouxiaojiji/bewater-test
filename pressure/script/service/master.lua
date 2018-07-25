512f1be7c9387f63271fc4d494c9d6bbe6aebf2c
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2018-07-25 11:28:56 +0800

    snax创建robot

--- pressure/script/service/master.lua
+++ pressure/script/service/master.lua
@@ -1,15 +1,16 @@
 local skynet    = require "skynet"
+local snax      = require "skynet.snax"
 local conf      = require "conf"
 
 skynet.start(function()
-    for i = 1, 16 do
+    --[[for i = 1, 1 do
         local robot = skynet.newservice("ws/robot", "player_t")
         --skynet.call(robot, "lua", "start", "ws://127.0.0.1:8002")
         skynet.call(robot, "lua", "start", "ws://127.0.0.1:8655")
         skynet.sleep(100)
-    end
-
-    --[[local player_t = require "player_t"
-    local player = player_t.new()
-    player:on_open()]]
+    end]]
+    --snax.newservice("robot", "player_t", "ws://127.0.0.1:8566")
+    --snax.newservice("robot", "player_t", "ws://127.0.0.1:8002")
+    local robot = skynet.newservice("ws/robot", "player_t")
+    skynet.call(robot, "lua", "start", "ws://127.0.0.1:8566")
 end)
