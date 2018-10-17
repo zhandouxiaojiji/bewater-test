e06790daf9ba90edb119eda298306b6e88be783e
huangjx <zhandouxiaojiji@gmail.com>
2018-10-17 10:50:44 +0800

    test timer

--- service/test.lua
+++ service/test.lua
@@ -3,13 +3,13 @@ local gmcmd     = require "gmcmd"
 local pressure  = require "pressure"
 
 function test_timer()
-    local timer_t = require "util.timer_t"
-    local timer = timer_t.new()
+    local timer = require "timer"
+    local ti = timer.create()
     math.randomseed(os.time())
     for i = 1, 50000 do
         local expire = math.random(1, 100)
         local start_time = skynet.time()
-        timer:delay(expire, function()
+        ti:delay(expire, function()
             print(string.format("test cd expire:%s use:%s", expire, skynet.time()-start_time))
         end)
         skynet.sleep(1)
@@ -19,6 +19,6 @@ end
 
 skynet.start(function()
     --gmcmd.test()
-    pressure.test("animal", 10, 2000)
-    --test_timer()
+    --pressure.test("animal", 10, 2000)
+    test_timer()
 end)
